657db3daa7a27200d5baa7c42586f9470f09f71d
Alexander Kluth <derhartmut@niwohlos.org>
2013-04-08 22:08:17 +0200

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Niwohgitadmin::Application.config.secret_token = 'f2f9a5f2018c10f0d5ab88daaa106585108286618d99c6422c79d6441ab97673f8ef090e74d74b3bb9cf7a140ac53182338968c2a2dd024cae67b234d18ad037'
